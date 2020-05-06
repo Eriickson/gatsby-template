25ca48fdb03cc5b9c902e1543766e1021491782c
Erickson - Casa <erickson01d@gmail.com>
2020-05-06 11:26:58 -0400

    agregando tailwincss con postcss

---
+++ src/pages/page-3.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Page3 = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold bg-red-200 grid grid-cols-12">
+        Pagina 3
+      </h1>
+    </div>
+  )
+}
+
+export default Page3
